92aeac6801d7aa0e125a327bd67a2033f2dbf676
NOTLAZERY <109110674+NOTLAZERY@users.noreply.github.com>
2023-01-15 13:21:47 +0200

    quick fix

--- kiril menu.lua
+++ kiril menu.lua
@@ -11,20 +11,21 @@ local currentver
 async_http.innit("raw.githubusercontent.com", "NOTLAZERY/kiril-menu/main/version.txt")
     current_version = tonumber(output)
     if local version ~= current_version then
-       util_toast("kiril menu", "new version available",)
-       menu.action(menu.my_root(), "update", {}, "", function()
-        async_http.init('raw.githubusercontent.com', 'NOTLAZERY/kiril-menu/main/kiril-menu.lua', function(a))
-            local err = select(2, load(a))
-            if err then
-                util_toast("error loading update", "if the error download the update manually",)
-            return end
-            local fn io.open(filesystem.scripts.dir().. SCRIPT_RELPATH, "wb")
-            f:write(a)
-            f:close()
-            util_toast("kiril menu", "updated",)
-            util.restart_script()
-        end)
-        async_http.dispatch()
+        util_toast("kiril menu", "new version available",)
+        menu.action(menu.my_root(), "update", {}, "", function()
+            async_http.init('raw.githubusercontent.com', 'NOTLAZERY/kiril-menu/main/kiril-menu.lua', function(a))
+                local err = select(2, load(a))
+                if err then
+                    util_toast("error loading update", "if the error download the update manually",)
+                return end
+                local fn io.open(filesystem.scripts.dir().. SCRIPT_RELPATH, "wb")
+                f:write(a)
+                f:close()
+                util_toast("kiril menu", "updated",)
+                util.restart_script()
+            end)
+            async_http.dispatch()
+        end
     end
 end, function() response = true end)
 async_http.dispatch()
