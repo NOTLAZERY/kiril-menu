1d3ba0f74f3a544b0586eb69b24a497811c11607
NOTLAZERY <109110674+NOTLAZERY@users.noreply.github.com>
2023-01-15 13:29:39 +0200

    bug fixes again

--- kiril menu.lua
+++ kiril menu.lua
@@ -1,16 +1,16 @@
-util.require_natives("natives-1663599433-uno")
+util.require_natives("natives-1627063482")
 util.require_natives("json")
 util.require_natives("lua_imGUI V2-1")
-util.require_natives("natives-1627063482")
 util.require_natives("")
 
 -- github version check and update
 local response = false
-local localver = "1.0.0"
-local currentver
+local localVer = 1.0.3
+local currentVer
 async_http.innit("raw.githubusercontent.com", "NOTLAZERY/kiril-menu/main/version.txt")
     current_version = tonumber(output)
-    if local version ~= current_version then
+    response = true
+    if local version ~= currentVer then
         util_toast("kiril menu", "new version available",)
         menu.action(menu.my_root(), "update", {}, "", function()
             async_http.init('raw.githubusercontent.com', 'NOTLAZERY/kiril-menu/main/kiril-menu.lua', function(a))
